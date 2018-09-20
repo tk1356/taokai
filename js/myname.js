c6c4d4f851651f3c39bdce82297bb31681d249b8
tk1356 <345650851@qq.com>
2018-09-20 10:07:21 +0800

    132

--- js/myname.js
+++ js/myname.js
@@ -11,16 +11,43 @@ function getTop(clsName){
         }
         var scrollT=document.documentElement.scrollTop||document.body.scrollTop;
         var bannerHeight = document.documentElement.clientHeight || document.body.clientHeight;
+function is_mobile() { 
+    var regex_match = /(nokia|iphone|android|motorola|^mot-|softbank|foma|docomo|kddi|up.browser|up.link|htc|dopod|blazer|netfront|helio|hosin|huawei|novarra|CoolPad|webos|techfaith|palmsource|blackberry|alcatel|amoi|ktouch|nexian|samsung|^sam-|s[cg]h|^lge|ericsson|philips|sagem|wellcom|bunjalloo|maui|symbian|smartphone|midp|wap|phone|windows ce|iemobile|^spice|^bird|^zte-|longcos|pantech|gionee|^sie-|portalmmm|jigs browser|hiptop|^benq|haier|^lct|operas*mobi|opera*mini|320x320|240x320|176x220)/i; 
+    var u = navigator.userAgent; 
+    if (null == u) { 
+        return true; 
+    } 
+    var result = regex_match.exec(u); 
+    if (null == result) { 
+        return false 
+    } else { 
+        return true 
+    } 
+}
+if (is_mobile()) {
+    $(document).ready(function() {
+        $(".s1 h3").removeClass('animated1');
+        $('.s1-h3').removeClass('animated1');
+        $('.s1-h4').removeClass('animated1');
+        $('.row1').removeClass('animated1');
+        $('.row2').removeClass('animated1');
+        $('.row3').removeClass('animated1');
+        $('.row4').removeClass('animated1');
+    });
+}
         window.addEventListener('scroll',function(){  
             /*business的触发*/
-            if(getTop('s1')< bannerHeight-100 ){
-                getDom('s1-h3').classList.add('done1');
-                getDom('s1-h4').classList.add('done1');
-                getDom('row1').classList.add('done2');
-                getDom('row2').classList.add('done3');
-                getDom('row3').classList.add('done4');
-                getDom('row4').classList.add('done5');
+            if (!is_mobile()) {
+                if(getTop('s1')< bannerHeight-100 ){
+                    getDom('s1-h3').classList.add('done1');
+                    getDom('s1-h4').classList.add('done1');
+                    getDom('row1').classList.add('done2');
+                    getDom('row2').classList.add('done3');
+                    getDom('row3').classList.add('done4');
+                    getDom('row4').classList.add('done5');
+                }
             }
+            
             if(getTop('s2')< bannerHeight-200){
                 getDom('s2-h3').classList.add('cone1');
                 getDom('s2-h4').classList.add('cone1');
