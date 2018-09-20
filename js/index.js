fceceb49cf8b64cbdf4b45cea460825302bdb0a6
tk1356 <345650851@qq.com>
2018-09-20 15:30:53 +0800

    123

--- js/index.js
+++ js/index.js
@@ -121,7 +121,7 @@ timer=setInterval(nextimg,4000);
 // 返回顶部
 function pageScroll(){
 //把内容滚动指定的像素数（第一个参数是向右滚动的像素数，第二个参数是向下滚动的像素数）
-    window.scrollBy(0,-100);
+    window.scrollBy(0,-500);
 //延时递归调用，模拟滚动向上效果
     scrolldelay = setTimeout('pageScroll()',100);
 //获取scrollTop值，声明了DTD的标准网页取document.documentElement.scrollTop，否则取document.body.scrollTop；因为二者只有一个会生效，另一个就恒为0，所以取和值可以得到网页的真正的scrollTop值
